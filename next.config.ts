da2634d9f675c380425f590a0c738e15bc7474d2
vamsi1225 <vamsi.morisetty@gmail.com>
2025-12-23 22:52:03 -0500

    Added certifications section and fixed errors

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'cdn.simpleicons.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.credly.com',
       },
     ],
   },
